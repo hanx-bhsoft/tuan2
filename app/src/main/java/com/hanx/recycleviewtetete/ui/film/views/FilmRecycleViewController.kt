e00526da98649200b9c17781b61c42e485783a29
Nguyễn Xuân Hà <hanx.bhsoft@gmail.com>
2024-12-24 15:26:28 +0700

    feat: week2 recycle view, navigation, string res, common views

--- app/src/main/java/com/hanx/recycleviewtetete/ui/film/views/FilmRecycleViewController.kt
+++ app/src/main/java/com/hanx/recycleviewtetete/ui/film/views/FilmRecycleViewController.kt
@@ -1,17 +1,15 @@
 package com.hanx.recycleviewtetete.ui.film.views
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.navigation.NavController
 import androidx.recyclerview.widget.RecyclerView
-import com.hanx.recycleviewtetete.MainActivity
 import com.hanx.recycleviewtetete.R
+import com.hanx.recycleviewtetete.type.Tasks
 
-class FilmRecycleViewController(var callBack: (String) -> Unit) : RecyclerView.Adapter<FilmRecycleViewController.ViewHolder>() {
-     var filmList = mutableListOf("film 1","film 2","film 3","film 4","film 5","film 6");
+class FilmRecycleViewController(val tasks:List<Tasks>, var callBack: (Tasks) -> Unit) : RecyclerView.Adapter<FilmRecycleViewController.ViewHolder>() {
+
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val filmName = view.findViewById<TextView>(R.id.filmName)
@@ -28,20 +26,18 @@ class FilmRecycleViewController(var callBack: (String) -> Unit) : RecyclerView.A
 
         val view =
             LayoutInflater.from(p0.context).inflate(if (p1%2== 0)R.layout.sample_fiml_item_view else R.layout.item_type2 , p0, false)
-                    ;
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int =
-        filmList.count()
+        this.tasks.count()
 
     override fun getItemViewType(position: Int): Int {
         return position%2
     }
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        println("intt vi")
-            p0.bind(filmList[p1]) {
-            callBack(filmList[p1])
+    override fun onBindViewHolder(p0: ViewHolder, index: Int) {
+            p0.bind(this.tasks[index].name) {
+            callBack(this.tasks[index])
         }
     }
 }
